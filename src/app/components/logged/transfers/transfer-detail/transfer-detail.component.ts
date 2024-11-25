1c2af3d40363f8975b6456b177f8954497634d10
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-11-25 00:40:17 -0300

    refactor: fix merge and bump version

--- src/app/components/logged/transfers/transfer-detail/transfer-detail.component.ts
+++ src/app/components/logged/transfers/transfer-detail/transfer-detail.component.ts
@@ -1,6 +1,11 @@
 import { DatePipe, DecimalPipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { TransactionResponse } from '../../../../models/transaction-response.model';
+import { AuthService } from '../../../../services/auth.service';
+import { JwtUser } from '../../../../models/jwt-user.model';
+import { AccountResponse } from '../../../../models/account-response.model';
+import { AccountService } from '../../../../services/account.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-transfer-detail',
@@ -9,6 +14,25 @@ import { TransactionResponse } from '../../../../models/transaction-response.mod
   templateUrl: './transfer-detail.component.html',
   styleUrl: './transfer-detail.component.css',
 })
-export class TransferDetailComponent {
+export class TransferDetailComponent implements OnInit, OnDestroy {
   @Input() transaction: TransactionResponse;
+  account: AccountResponse;
+  subscription: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private accountService: AccountService
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription = this.authService.jwtUser$.subscribe((jwtuser) => {
+      this.accountService.findById(jwtuser.accountId).subscribe((account) => {
+        this.account = account;
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
